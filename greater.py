fb91202de1569205197f2a322ab89ce8ac8564b0
nishantkumarr <nishanttdevops@gmail.com>
2025-04-06 04:24:25 +0000

    python

---
+++ greater.py
@@ -0,0 +1,41 @@
+a=10
+b=200
+c=50
+d=30
+
+# if a>b:
+#     print("a bada h")
+# else:
+#     print("b is greater")
+
+# if a>b:
+#     if a>c:
+#         print("a is greater among three")
+#     else:
+#         print("c is greater among three")
+# else:
+#     if b>c:
+#         print("b is greater ")
+#     else:
+#         print("c is greater")
+
+if (a>=b) and (a>=c):
+    print("a is greater")
+elif (b>=c):
+     print("b is greater")
+else:
+     print("c is greater")    
+
+
+
+# if a>b:
+#     if b>c:
+#         if c>d:
+#             print("c bada h") 
+#         else:
+#             print("b is greater") 
+#     else:
+#         print("c is greater") 
+# else:
+#     print("d is greater")    
+
---
+++ swap.py
@@ -0,0 +1,13 @@
+a=5
+b=10
+
+# c = a
+# a = b
+# b = c
+
+a = a+b
+b = a-b
+a = a-b
+print("a ki value", a)
+print("b ki value", b)
+
