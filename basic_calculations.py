ca89bd2aca49ac06538daf6c88990c87bb772377
nishantkumarr <nishanttdevops@gmail.com>
2025-08-13 16:25:05 +0000

    update

---
+++ basic_calculations.py
@@ -0,0 +1,31 @@
+a = 546
+b = 46  
+
+print("remainder aaega", a % b)
+
+
+km = float(input("enter value of kilometer:"))
+miles = km * 0.621371
+print(km ,"kilometer ")
+print(miles , "kilometer convart to miles")
+
+
+km = float(input("enter value of kilometer:"))
+square = km *1195990.05
+print (km,"kilometer")
+print (square,"kilometer convart to square")
+
+
+a = 5
+b = 5
+print(a-b)
+print(a+b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+
+
+num = 10
+num /= 25   
+print("num :",num)
