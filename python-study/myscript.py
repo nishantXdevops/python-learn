3ec38f3be9168169d879982cb4dc152a8ca887a2
nishantkumarr <nishanttdevops@gmail.com>
2026-02-14 07:03:53 +0000

    upload python-study

---
+++ python-study/myscript.py
@@ -0,0 +1,12 @@
+
+# print ("my name is nishant" ,"i am from barauth currently")
+
+# print (34-34)
+
+name = "nishasnt" 
+age = 20
+price = 25.66
+age2 = age
+print ("my name is",name )
+print ("my age is",age2 )
+print ("price",price)
---
+++ python-study/swap.py
@@ -0,0 +1,17 @@
+# a=5
+# b=10
+
+# # c = a
+# # a = b
+# # b = c
+
+# a = a+b
+# b = a-b
+# a = a-b
+# print("a ki value", a)
+# print("b ki value", b)
+
+a = int(input("enter a number:"))
+b = int(input("enter of another value:"))
+sum = a + b
+print("The sum of a and b is:", sum)
