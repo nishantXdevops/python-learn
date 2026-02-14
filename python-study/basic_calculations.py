c01c42be618b4977fc079d11082d182dcd338752
nishantkumarr <nishanttdevops@gmail.com>
2026-02-14 07:30:29 +0000

    upload code

---
+++ python-study/basic_calculations.py
@@ -0,0 +1,111 @@
+a = 546
+b = 46  
+
+print("remainder aaega", a % b)
+
+
+km = float(input("enter value of kilometer:"))
+miles = km * 0.621371
+print(km ,"kilometer ")
+print(miles , "kilometer convart to miles")
+
+
+km = float(input("enter value of kilometer:"))
+square = km *1195990.05
+print (km,"kilometer")
+print (square,"kilometer convart to square")
+
+#--------------------------------------------------------------------------------
+
+a = 5
+b = 5
+print(a-b)
+print(a+b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+
+
+num = 10
+num /= 25   
+print("num :",num)
+
+#-------------------------------------------------------------------------------
+
+
+i =20
+if(i > 15):
+  print ("10 is less then 15")
+else:
+  print('elseeee')
+  print("i am not in if")   
+
+#--------------------------------------------------------------------------------
+
+
+x = 10
+
+if x ==5:
+    print("x बड़ा है 5 से")
+
+elif x ==5:
+    print("x बराबर है 5 के")
+
+else:
+    print("x छोटा है 5 से")
+
+
+#--------------------------------------------------------------------------------
+
+i = 10
+j = 5
+
+if i < 11:
+    print("Condition 1 is true")
+
+if j == 5:
+    print("Condition 2 is true")
+
+if i == 10 or j > 7:
+    print("operater")
+else:
+    print("no operater")
+
+
+#--------------------------------------------------------------------------------    
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+c = a + b
+print("The sum of a and b is:", c)
+
+#--------------------------------------------------------------------------------
+
+a = 4
+b = 5
+c = 6
+
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+else:
+    print(c)
+
+#--------------------------------------------------------------------------------
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+d = int(input("Enter fourth number: "))
+e = int(input("Enter fifth number: "))
+
+print("Largest number:", max(a, b, c, d, e))
+
+
+
+#--------------------------------------------------------------------------------
+
+
+
--- python-study/greater.py
+++ python-study/greater.py
@@ -3,72 +3,39 @@ b=200
 c=50
 d=30
 
-if a<b:
-    print("a bada h")
-else:
-    print("b bda h")
-
-# # if a>b:
-# #     if a>c:
-# #         print("a is greater among three")
-# #     else:
-# #         print("c is greater among three")
-# # else:
-# #     if b>c:
-# #         print("b is greater ")
-# #     else:
-# #         print("c is greater")
-
-# if (a>=b) and (a>=c):
-#     print("a is greater")
-# elif (b>=c):
-#      print("b is greater")
+# if a>b:
+#     print("a bada h")
 # else:
-#      print("c is greater")    
+#     print("b is greater")
 
+# if a>b:
+#     if a>c:
+#         print("a is greater among three")
+#     else:
+#         print("c is greater among three")
+# else:
+#     if b>c:
+#         print("b is greater ")
+#     else:
+#         print("c is greater")
 
-# # if a>b:
-# #     if b>c:
-# #         if c>d:
-# #             print("c bada h") 
-# #         else:
-# #             print("b is greater") 
-# #     else:
-# #         print("c is greater") 
-# # else:
-# #     print("d is greater")    
-
-
-
-from PIL import Image
-import pytesseract
-from pdf2image import convert_from_path
-
-# Corrected file path with forward slashes (Linux-style)
-pdf_path = "/mnt/c/Users/pk226/Downloads/4003_23.pdf"
-
-# Convert PDF pages to images
-images = convert_from_path(pdf_path)
-
-# Use OCR to extract text from each image (English + Hindi)
-text_pages = [pytesseract.image_to_string(img, lang='eng+hin') for img in images]
-
-# Combine all extracted text into one string
-full_text = "\n\n".join(text_pages)
-
-# Preview the first 3000 characters (optional for debugging)
-print(full_text[:3000])
+if (a>=b) and (a>=c):
+    print("a is greater")
+elif (b>=c):
+     print("b is greater")
+else:
+     print("c is greater")    
 
 
 
-
-
-
-
-
-
-
-
-
-
+# if a>b:
+#     if b>c:
+#         if c>d:
+#             print("c bada h") 
+#         else:
+#             print("b is greater") 
+#     else:
+#         print("c is greater") 
+# else:
+#     print("d is greater")    
 
